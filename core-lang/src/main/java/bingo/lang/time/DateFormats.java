23753fda62fcee1ccbc68a05be0285a6a5285091
fenghm <live.fenghm@gmail.com>
2012-05-02 14:07:25 +0800

    move package bingo.lang.format to bingo.lang.time

--- core-lang/src/main/java/bingo/lang/time/DateFormats.java
+++ core-lang/src/main/java/bingo/lang/time/DateFormats.java
@@ -22,7 +22,6 @@ import java.text.DateFormat;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import bingo.lang.format.ConcurrentDateFormat;
 
 
 public class DateFormats {
--- core-lang/src/main/java/bingo/lang/time/DurationFormatter.java
+++ core-lang/src/main/java/bingo/lang/time/DurationFormatter.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package bingo.lang.format;
+package bingo.lang.time;
 
 import java.util.ArrayList;
 
