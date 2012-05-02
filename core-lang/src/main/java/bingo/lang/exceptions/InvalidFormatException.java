5c1b5ced69b19310255d8410fc2b5fb3cb0f1d40
fenghm <live.fenghm@gmail.com>
2012-05-02 14:17:49 +0800

    add three exception classes

---
+++ core-lang/src/main/java/bingo/lang/exceptions/InvalidFormatException.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package bingo.lang.exceptions;
+
+public class InvalidFormatException extends RuntimeExceptionEx {
+
+	private static final long serialVersionUID = -15305099775642352L;
+
+	public InvalidFormatException() {
+	    super();
+    }
+
+	public InvalidFormatException(String message, Object... args) {
+	    super(message, args);
+    }
+
+	public InvalidFormatException(String message, Throwable cause) {
+	    super(message, cause);
+    }
+
+	public InvalidFormatException(String message) {
+	    super(message);
+    }
+
+	public InvalidFormatException(Throwable cause, String message, Object... args) {
+	    super(cause, message, args);
+    }
+
+	public InvalidFormatException(Throwable cause) {
+	    super(cause);
+    }
+}
---
+++ core-lang/src/main/java/bingo/lang/exceptions/InvalidOperationException.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package bingo.lang.exceptions;
+
+public class InvalidOperationException extends RuntimeExceptionEx {
+
+	private static final long serialVersionUID = -133819354586484655L;
+
+	public InvalidOperationException() {
+		
+	}
+
+	public InvalidOperationException(String message) {
+		super(message);
+	}
+
+	public InvalidOperationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public InvalidOperationException(String message, Object... args) {
+		super(message, args);
+	}
+
+	public InvalidOperationException(Throwable cause) {
+		super(cause);
+	}
+
+	public InvalidOperationException(Throwable cause, String message, Object... args) {
+		super(cause, message, args);
+	}
+}
