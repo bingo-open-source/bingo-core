ada4bb0461f3fff21f9c7c7d0aca6a8958d49255
CalvinChen <Calvin.Chen.310@gmail.com>
2012-05-22 13:20:33 +0800

    add javadoc to ReflectMember.java.

--- core-lang/src/main/java/bingo/lang/reflect/ReflectMember.java
+++ core-lang/src/main/java/bingo/lang/reflect/ReflectMember.java
@@ -20,26 +20,57 @@ import java.lang.reflect.Modifier;
 
 import bingo.lang.Named;
 
+/**
+ * 反射类的成员变量
+ */
 public abstract class ReflectMember implements Named {
 
+	/**
+	 * 所对应的反射类
+	 */
 	protected final ReflectClass<?> reflectClass;
+	
+	/**
+	 * 是否在所在反射类中被声明
+	 */
 	protected final boolean	      isDeclared;
+	
+	/**
+	 * 是否为公共变量
+	 */
 	protected final boolean		  isPublic;
 	
+	/**
+	 * 初始化成员变量。
+	 * @param reflectClass 所对应的反射类
+	 * @param member jdk中的成员变量类
+	 */
 	protected ReflectMember(ReflectClass<?> reflectClass,Member member){
 		this.reflectClass = reflectClass;
 		this.isDeclared   = member.getDeclaringClass() == reflectClass.getJavaClass();
 		this.isPublic     = Modifier.isPublic(member.getModifiers());
 	}
 
+	/**
+	 * 获得此成员变量对应的反射类。
+	 * @return 此成员变量对应的反射类。
+	 */
 	public ReflectClass<?> getReflectClass() {
     	return reflectClass;
     }
 	
+	/**
+	 * 是否在所在反射类中被声明。
+	 * @return 如果在所在反射类中被声明，返回true，否则返回false。
+	 */
 	public boolean isDeclared() {
     	return isDeclared;
     }
 	
+	/**
+	 * 是否为公共成员变量。
+	 * @return 如果为公共成员变量，返回true，否则返回false。
+	 */
 	public boolean isPublic(){
 		return isPublic;
 	}
