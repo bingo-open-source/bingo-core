0483c7e5174b073ba2a0cb4c2b1799fc42d072b8
CalvinChen <Calvin.Chen.310@gmail.com>
2012-05-30 11:29:37 +0800

    chang using bingo.lang.xml instead of dom4j.

--- core-lang/src/main/java/bingo/lang/testing/PerfHtmlBuilder.java
+++ core-lang/src/main/java/bingo/lang/testing/PerfHtmlBuilder.java
@@ -2,72 +2,133 @@ package bingo.lang.testing;
 
 import java.util.Date;
 
-import org.dom4j.Document;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
+import bingo.lang.xml.XmlAttribute;
+import bingo.lang.xml.XmlElement;
+import bingo.lang.xml.XmlText;
 
 public class PerfHtmlBuilder {
 	
-	public static Document buildUpMatrixHtml(String projectName, PerfResult[][] resultMatrix) {
-		Document doc = DocumentHelper.createDocument();
-    	Element html = doc.addElement("html");
-    	html.addAttribute("lang", "en");
+	public static XmlElement buildUpMatrixHtml(String projectName, PerfResult[][] resultMatrix) {
+		XmlElement html = new XmlElement("html", new XmlAttribute("lang", "en"));
     	buildUpHead(projectName, html);
     	buildUpBody(projectName, html, resultMatrix);
-	    return doc;
+	    return html;
     }
 	
-	private static Element buildUpHead(String projectName, Element html){
-    	Element head = html.addElement("head");
-    	head.addElement("title").addText("The Performance Report of \"" + projectName + "\"");
-    	Element link = head.addElement("link");
-    	link.addAttribute("href", "http://twitter.github.com/bootstrap/assets/css/bootstrap.css");
-    	link.addAttribute("rel", "stylesheet");
-    	head.addElement("style").addText("body{padding:20px}");
+	public static XmlElement buildUpGroupHtml(String projectName, PerfResult perfResult) {
+		XmlElement html = new XmlElement("html", new XmlAttribute("lang", "en"));
+    	buildUpHead(projectName, html);
+    	buildUpGroupBody(projectName, html, perfResult);
+	    return html;
+	}
+	
+    
+    private static XmlElement buildUpGroupBody(String projectName, XmlElement html, PerfResult perfResult){
+    	XmlElement tbody = null;
+    	XmlElement body = new XmlElement("body", 
+    		new XmlElement("h2", new XmlText(projectName)),
+    		new XmlElement("table", 
+    			new XmlAttribute("class", "table table-bordered table-striped"),
+    			new XmlElement("thead", 
+    				new XmlElement("tr", 
+    					new XmlElement("th", new XmlText("Hierarchy")),
+    					new XmlElement("th", new XmlText("Name")),
+    					new XmlElement("th", new XmlText("Run Times")),
+    					new XmlElement("th", new XmlText("ms")),
+    					new XmlElement("th", new XmlText("ns"))
+    					)),
+    			tbody = new XmlElement("tbody")));
+
+    	buildUpGroupTbody("|", 0, perfResult, tbody);
+    	html.add(body);
+    	return body;
+    }
+    
+    private static void buildUpGroupTbody(String prefix, int number, PerfResult perfResult, XmlElement tbody){
+    	if(number != 0){
+        	XmlElement tr = new XmlElement("tr", 
+            		new XmlElement("td", new XmlText(prefix)),
+            		new XmlElement("td", new XmlText(perfResult.getName())),
+            		new XmlElement("td", new XmlText(perfResult.getRunTimes() + "")),
+            		new XmlElement("td", new XmlText(perfResult.getElapsedMilliseconds() + "")),
+            		new XmlElement("td", new XmlText(perfResult.getElapsedNanoseconds() + ""))
+        		);
+    		prefix += "--" + number + "--|";
+        	tbody.add(tr);
+    	}
+    	number++;
+    	
+    	if(perfResult.isEnd() == false){
+    		for (PerfResult perf : perfResult.getChildren()) {
+    			buildUpGroupTbody(prefix, number, perf, tbody);
+            }
+    	}
+    }
+	
+	private static XmlElement buildUpHead(String projectName, XmlElement html){
+    	XmlElement head = 
+    		new XmlElement("head", 
+    			new XmlElement("title", 
+    				new XmlText("The Performance Report of \"" + projectName + "\"")),
+				new XmlElement("link", 
+	    			new XmlAttribute("href", "http://twitter.github.com/bootstrap/assets/css/bootstrap.css"),
+	    			new XmlAttribute("rel", "stylesheet")),
+		    	new XmlElement("style", new XmlText("body{padding:20px}")));
+    	html.add(head);
     	return head;
 	}
 	
-	private static Element buildUpBody(String projectName, Element html, PerfResult[][] resultMatrix){
-		Element body = html.addElement("body");
-    	Element divContainer = body.addElement("div");
-    	divContainer.addAttribute("class", "containner");
-    	divContainer.addElement("H1").addText(projectName);
-    	buildUpTable(divContainer, resultMatrix);
-    	Element block = divContainer.addElement("blockquote");
-    	block.addAttribute("class", "pull-right");
-    	block.addElement("p").addText("Generated on " + new Date().toString() + ".");
-    	block.addElement("small").addText("powered by bingo.core");
+	private static XmlElement buildUpBody(String projectName, XmlElement html, PerfResult[][] resultMatrix){
+		XmlElement divContainer = null;
+		XmlElement body = 
+			new XmlElement("body", 
+ divContainer = new XmlElement("div", 
+				new XmlAttribute("class", "containner"),
+				new XmlElement("h1", new XmlText(projectName))));
+    	buildUpMatrixTable(divContainer, resultMatrix);
+    	divContainer.add(
+	    	new XmlElement("blockquote",
+	    		new XmlAttribute("class", "pull-right"),
+	    		new XmlElement("p", new XmlText("Generated on " + new Date().toString() + ".")),
+	    		new XmlElement("small", new XmlText("powered by bingo.core"))));
+    	html.add(body);
     	return body;
 	}
 	
-	private static Element buildUpTable(Element divContainer, PerfResult[][] resultMatrix){
-		Element table = divContainer.addElement("table");
-    	table.addAttribute("class", "table table-bordered table-striped");
-    	Element thead = table.addElement("thead");
-    	Element tr = thead.addElement("tr");
-    	tr.addElement("th").addText("Repeat Time\\Code");
+	private static XmlElement buildUpMatrixTable(XmlElement divContainer, PerfResult[][] resultMatrix){
+		XmlElement tr = null;
+		XmlElement table = 
+			new XmlElement("table", 
+				new XmlAttribute("class", "table table-bordered table-striped"),
+				new XmlElement("thead", 
+					tr = new XmlElement("tr")));
+		tr.add(new XmlElement("th", new XmlText("Repeat Time\\Code")));
     	for (int i = 0; i < resultMatrix[0].length; i++) {
-	        tr.addElement("th").addText(resultMatrix[0][i].getName());
+	        tr.add(new XmlElement("th", new XmlText(resultMatrix[0][i].getName())));
         }
-    	buildUpTbody(table, resultMatrix);
+    	buildUpMatrixTbody(table, resultMatrix);
+    	divContainer.add(table);
     	return table;
 	}
 	
-	private static Element buildUpTbody(Element table, PerfResult[][] resultMatrix){
-		Element tbody = table.addElement("tbody");
-		Element tr = null;
+	private static XmlElement buildUpMatrixTbody(XmlElement table, PerfResult[][] resultMatrix){
+		XmlElement tbody = new XmlElement("tbody");
+		XmlElement tr = null;
     	for (int i = 0; i < resultMatrix.length; i++) {
-    		tr = tbody.addElement("tr");
-    		tr.addElement("td").addText("")
-    		  .addElement("strong").addText("" + resultMatrix[i][0].getRunTimes());
+    		tr = new XmlElement("tr");
+    		tr.add(new XmlElement("td", 
+    				new XmlElement("strong", new XmlText(resultMatrix[i][0].getRunTimes() + ""))));
 	        for (int j = 0; j < resultMatrix[i].length; j++) {
 	        	PerfResult cell = resultMatrix[i][j];
-	            tr.addElement("td").addText("")
-	            .addElement("strong").addText(cell.getElapsedNanoseconds() + "")
-	            .getParent().addText(" ns.(" + cell.getElapsedMilliseconds() + 
-	            		" ms, " + (int)(cell.getElapsedNanoseconds()*1.0/cell.getRunTimes() + 0.5) + " ns each)");
+	    		tr.add(new XmlElement("td", 
+	    				new XmlElement("strong", new XmlText(cell.getElapsedNanoseconds() + "")),
+	    				new XmlText(" ns.(" + cell.getElapsedMilliseconds() + " ms, "
+	    						+ (int)(cell.getElapsedNanoseconds()*1.0/cell.getRunTimes() + 0.5)
+	    						+ " ns each)")));
             }
+	        tbody.add(tr);
         }
+    	table.add(tbody);
     	return tbody;
 	}
 }
